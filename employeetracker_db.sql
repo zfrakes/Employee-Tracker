b07c5b2ff30b7286eb6f524d256c9416605143f3
Zoey Frakes <zfrakes811@gmail.com>
2020-02-16 17:37:47 -0500

    update sql with employee'sinformation

--- employeetracker_db.sql
+++ employeetracker_db.sql
@@ -1,12 +1,10 @@
+DROP DATABASE IF EXISTS employeetracker_db
 CREATE DATABASE employeetracker_db;
-
 USE employeetracker_db;
 
 CREATE TABLE department(
-
- id INTEGER PRIMARY KEY
- 
- name VARCHAR(30),
+id INTEGER PRIMARY KEY
+name VARCHAR(30),
 );
 
 CREATE TABLE empolyee_role (
@@ -23,3 +21,15 @@ CREATE TABLE empolyee (
   role_id INTEGER,
   manager_id INTEGER
 );
+
+
+
+
+INSERT INTO empolyee (firstName, lastName,role_id, manager_id) values ('john', 'Doe','Sales Lead','Ashley Rodriguez');
+INSERT INTO empolyee (firstName, lastName,role_id, manager_id) values ('Mike', 'Chan','Salesperson','John Doe');
+INSERT INTO empolyee (firstName, lastName,role_id, manageer_id) values ('Ashley', 'Rodriguez','lead Engieering');
+INSERT INTO empolyee (firstName, lastName,role_id, manager_id) values ('Kevin', 'Tupik','Software Engineer','Asheley Rodriguez');
+INSERT INTO empolyee (firstName, lastName,role_id, manager_id) values ('Malia', 'Brown','Accountant');
+INSERT INTO empolyee (firstName, lastName,role_id, manager_id) values ('Sarah', 'Lourd','legal Team Lead',);
+INSERT INTO empolyee (firstName, lastName,role_id, manager_id) values ('Tom', 'Allen','laywer','Sarah Lourd');
+INSERT INTO empolyee (firstName, lastName,role_id, manager_id) values ('Christian', 'Eckenrode','Lead Engieering','Mike Chan');
