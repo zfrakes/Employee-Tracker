63c51d85c125088793c481fd401980c5ecba4db7
Zoey Frakes <zfrakes811@gmail.com>
2020-02-22 16:52:11 -0500

    added functions for update employee role and update manager for employee

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const cTable = require('console.table');
-var inquirer = require('inquirer')
-var mysql = require('mysql');
-var connection = mysql.createConnection({
+const inquirer = require('inquirer')
+const  mysql = require('mysql');
+const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
@@ -11,7 +11,7 @@ var connection = mysql.createConnection({
 
 function employeeSearch() {
     // 1. Prints out all employees
-    var query = "SELECT * FROM employee";
+    const query = "SELECT * FROM employee";
     connection.query(query, function (err, res) {
         if (err) throw err;
         console.table(res);
@@ -22,7 +22,7 @@ function employeeSearch() {
 };
 function multiSearch() {
     // 1. Prints out all employees
-    var query = "SELECT * FROM department";
+    const query = "SELECT * FROM department";
     connection.query(query, function (err, res) {
         if (err) throw err;
         console.table(res);
@@ -110,7 +110,7 @@ function adddepartmentSearch() {
             }])
 
             .then(function (answer) {
-                var query = `INSERT INTO employee_role(title, salary, department_id) VALUES ("${answer.first_name}", ${answer.last_name}, ${answer.role_id}, ${answer.manager_id})`;
+                var query = `INSERT INTO employee (first_name, last_name, role_id,manager_id) VALUES ("${answer.first_name}", "${answer.last_name}", ${answer.role_id}, ${answer.manager_id})`;
                 connection.query(query, function (err, result) {
                     if (err) throw err;
                     console.log("1 record inserted");
@@ -118,47 +118,67 @@ function adddepartmentSearch() {
                 });
 
             })
+        }
             
             function  updatemployeeroleSearch() {
                 inquirer
                     .prompt([{
-                        name: "name",
+                        name: "id",
                         type: "input",
-                        message: "Which employee would you like to update their?"
-                    }, {
-                        name: "last_name",
-                        type: "input",
-                        message: "What is the employee updated role?"
-                    }, {
+                        message: "Which employee would you like to update (enter id)?"
+                    }, 
+                     {
                         name: "role_id",
                         type: "input",
-                        message: "What would you like the role to be (enter id)?"
-                    },
-            
-                    }]);
+                        message: "What would you like the updated role to be (enter id)?"
+                    }, 
+                    ])
             
             .then(function(answer) {
-                if (err) throw err;
-                var query = "UPDATE customers SET = 'Canyon 123' WHERE address = 'Valley 345'";
+                var query = `UPDATE employee SET role_id = ${answer.role_id} WHERE id = ${answer.id}`;
                 connection.query(query, function (err, result) {
                   if (err) throw err;
                   console.log(result.affectedRows + " record(s) updated");
                 });
               });
-    function runSearch() {
+            }
+            function updatemployeemanagerSearch() {
+                inquirer
+                    .prompt([{
+                        name: "id",
+                        type: "input",
+                        message: "Which employee would you like to update (enter id)?"
+                    }, 
+                     {
+                        name: "manager_id",
+                        type: "input",
+                        message: "Who would you like to be updated manager (enter id)?"
+                    }, 
+                    ])
+            
+            .then(function(answer) {
+                var query = `UPDATE employee SET manager_id = ${answer.manager_id} WHERE id = ${answer.id}`;
+                connection.query(query, function (err, result) {
+                  if (err) throw err;
+                  console.log(result.affectedRows + " record(s) updated");
+                });
+              });
+            }
+        function runSearch() {
         inquirer
             .prompt({
                 name: "action",
                 type: "rawlist",
                 message: "What would you like to do?",
                 choices: [
-                    "View All Employees",ou
+                    "View All Employees",
                     "View All Departments",
                     "View All Roles",
                     "Add Department",
                     "Add Role",
                     "Add Employee",
-                    "Update employee roles"
+                    "Update employee roles",
+                    "Update employee manager"
                 ]
             })
             .then(function (answer) {
@@ -190,10 +210,13 @@ function adddepartmentSearch() {
                     case "Update employee roles":
                         updatemployeeroleSearch();
                         break;
+                    case "Update employee manager":
+                        updatemployeemanagerSearch();
+                        break;
                 }
             });
     }
     connection.connect(function (err) {
         if (err) throw err;
         runSearch();
-    })
+    });
